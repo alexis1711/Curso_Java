b60c24e58afc845312198fde1788d2d8381eee46
Alexis Cornejo <53987686+alexis1711@users.noreply.github.com>
2022-09-13 11:26:38 -0600

    Add files via upload

---
+++ CursoDocker/curso-dockers/msvc-cursos/src/main/java/com/example/msvccursos/repositories/CursoRepository.java
@@ -0,0 +1,10 @@
+package com.example.msvccursos.repositories;
+
+import com.example.msvccursos.entity.Curso;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CursoRepository extends CrudRepository<Curso, Long> {
+
+
+
+}
---
+++ CursoDocker/curso-dockers/msvc-cursos/src/main/java/com/example/msvccursos/services/CursoService.java
@@ -0,0 +1,15 @@
+package com.example.msvccursos.services;
+
+import com.example.msvccursos.entity.Curso;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CursoService {
+
+    List<Curso> listar();
+    Optional<Curso> porId(Long id);
+    Curso guardar(Curso curso);
+    void eliminar(Long id);
+
+}
